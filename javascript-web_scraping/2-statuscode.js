320234575943412b096df9c3aee692dc77564731
Erkenoss <xavier.bertin6@gmail.com>
2024-01-22 11:11:22 +0100

    try fixed request

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,10 +3,8 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, (err, response) => {
-  if (err) {
-    console.error(err);
-  } else {
+request(url, (err, response, body) => {
+  if (!err) {
     console.log(`code: ${response.statusCode}`);
   }
 });

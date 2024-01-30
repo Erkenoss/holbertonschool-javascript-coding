3c2491f0b2ae6de48c9f000fa8c2d142231d400a
Erkenoss <xavier.bertin6@gmail.com>
2024-01-30 14:00:55 +0100

    add task 5-

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const databasePath = 'database.csv';
+const port = 1245;
+
+const app = http.createServer(async (req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!\n');
+  } else if (req.url === '/students') {
+    try {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      response = await countStudents(databasePath);
+      res.end(`This is the list of our students\n` + response);
+    } catch (error) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+      console.error(error);
+    }
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found\n');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
+module.exports = app;

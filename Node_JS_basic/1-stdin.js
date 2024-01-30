78b416ff77abcfa75838be2d2d42ef693c59aa4d
Erkenoss <xavier.bertin6@gmail.com>
2024-01-30 10:15:11 +0100

    add task 1-

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,17 +1,15 @@
 process.stdin.setEncoding('utf8');
 
-const string = "Welcome to Holberton School, what is your name?";
-process.stdout.write(string + "\n");
-process.stdin.on('readable', function() {
-
-
-var chunk = process.stdin.read();
-if (chunk !== null) {
-    process.stdout.write('Your name is: ' + chunk);
-    process.stdout.write("This important software is now closing" + "\n");
+const string = 'Welcome to Holberton School, what is your name?';
+process.stdout.write(`${string}\n`);
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+    process.stdout.write('This important software is now closing\n');
   }
 });
 
-process.stdin.on('end', function() {
+process.stdin.on('end', () => {
   process.stdout.write('end');
 });

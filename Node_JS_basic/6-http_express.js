3e62e9cf701f4b49a650f20c392e1b9f179661b1
Erkenoss <xavier.bertin6@gmail.com>
2024-01-30 14:09:13 +0100

    eslint

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 const port = 1245;
 
-app.get("/", function (req, res) {
-    res.send("Hello Holberton School!\n");
-  });
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!\n');
+});
 
-  app.listen(port, function () {
-    console.log(`Server running at http://localhost:${port}/!`);
-  });
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/!`);
+});

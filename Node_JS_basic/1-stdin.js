d1ca6abe3356c95935b4f52d70fbfe295e85a83c
Erkenoss <xavier.bertin6@gmail.com>
2024-02-01 10:28:41 +0100

    add task 1-

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -9,7 +9,3 @@ process.stdin.on('readable', () => {
     process.stdout.write('This important software is now closing\n');
   }
 });
-
-process.stdin.on('end', () => {
-  process.stdout.write('end');
-});

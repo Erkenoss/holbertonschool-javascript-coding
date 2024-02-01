d3d6de896a626c83c954f4eb15806ca21f71eeb5
Erkenoss <xavier.bertin6@gmail.com>
2024-02-01 11:31:03 +0100

    fixed task 3-

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -3,6 +3,10 @@ const fs = require('fs');
 function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (error, data) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+      }
+
       const lines = data.trim().split('\n');
 
       let csCount = 0;
@@ -13,26 +17,23 @@ function countStudents(path) {
       for (const line of lines) {
         const fields = line.split(',');
         const [firstName, lastName, age, field] = fields;
-        if (error)
-          reject(new Error('Cannot load the database'));
-        
+
         if (firstName && lastName && age && field) {
-            if (field === 'CS') {
-              csCount += 1;
-              csList.push(firstName);
-            } else if (field === 'SWE') {
-              sweCount += 1;
-              sweList.push(firstName);
-            }
+          if (field === 'CS') {
+            csCount += 1;
+            csList.push(firstName);
+          } else if (field === 'SWE') {
+            sweCount += 1;
+            sweList.push(firstName);
           }
         }
-
-        console.log(`Number of students: ${csCount + sweCount}`);
-        console.log(`Number of students in CS: ${csCount}. List: ${csList.join(', ')}`);
-        console.log(`Number of students in SWE: ${sweCount}. List: ${sweList.join(', ')}`);
-
-        resolve();
       }
+
+      console.log(`Number of students: ${csCount + sweCount}`);
+      console.log(`Number of students in CS: ${csCount}. List: ${csList.join(', ')}`);
+      console.log(`Number of students in SWE: ${sweCount}. List: ${sweList.join(', ')}`);
+
+      resolve();
     });
   });
 }
